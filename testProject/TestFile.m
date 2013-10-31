cce891717fe206490f5d0fcc70782b2e3d309191
tpgoranov <todor@Todors-MacBook-Pro.local>
2013-10-31 08:01:59 +0200

    dfvdfv

---
+++ testProject/TestFile.m
@@ -0,0 +1,13 @@
+//
+//  TestFile.m
+//  testProject
+//
+//  Created by Todor Goranov on 10/31/13.
+//  Copyright (c) 2013 Todor Goranov. All rights reserved.
+//
+
+#import "TestFile.h"
+
+@implementation TestFile
+
+@end
