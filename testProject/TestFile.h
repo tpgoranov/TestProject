93c52bcf675c4ed6203d00f7da6373dd12185fcd
tpgoranov <todor@Todors-MacBook-Pro.local>
2013-10-31 08:01:45 +0200

    dsfsf

---
+++ testProject/TestFile.h
@@ -0,0 +1,13 @@
+//
+//  TestFile.h
+//  testProject
+//
+//  Created by Todor Goranov on 10/31/13.
+//  Copyright (c) 2013 Todor Goranov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestFile : NSObject
+
+@end
